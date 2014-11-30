f805a8f8c39460310038dd70d495b6fdf1d16712
Joe Burgess <joe@flatironschool.com>
2014-11-30 09:35:57 -0500

    Finished solution

--- swift-CashMoneyBlocks/CashRegister.swift
+++ swift-CashMoneyBlocks/CashRegister.swift
@@ -9,5 +9,41 @@
 import UIKit
 
 class CashRegister: NSObject {
-   
+    let store : Store
+    let transactions : [Transaction]
+    let taxLogic : (transaction : Transaction) -> Float
+    let couponLogic : (transaction : Transaction) -> Float
+
+    init (store: Store, taxLogic : (transaction : Transaction) -> Float, couponLogic : (transaction : Transaction) -> Float)
+    {
+        self.store = store
+        self.taxLogic = taxLogic
+        self.couponLogic = couponLogic
+        self.transactions = [Transaction]()
+    }
+
+    func applyCoupons() -> Float
+    {
+        var totalSaved : Float = 0
+        for transaction in transactions
+        {
+            let saved = self.couponLogic(transaction: transaction)
+            totalSaved += saved
+        }
+
+        return totalSaved
+    }
+
+    func calculateTax() -> Float
+    {
+        var totalTax : Float = 0
+        for transaction in transactions
+        {
+            let tax = self.taxLogic(transaction: transaction)
+            totalTax += tax
+        }
+
+        return totalTax
+    }
+
 }
