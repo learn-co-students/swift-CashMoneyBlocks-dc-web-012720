41fecf02b62e3a7874aafb3bdf7850cf410579c6
Joe Burgess <joe@flatironschool.com>
2014-11-30 09:35:26 -0500

    Starting to write tests

---
+++ swift-CashMoneyBlocksTests/cashRegisterSpec.swift
@@ -0,0 +1,12 @@
+import Quick
+import Nimble
+import swift_CashMoneyBlocks
+
+class cashRegisterSpec: QuickSpec {
+    override func spec() {
+        beforeEach() {
+            let apple = Product(productDescription: "Apple", upc: "0000000000415", price: 0.95, measure: "ea", category: ProductCategory.Grocery)
+            
+        }
+    }
+}
